fdacae88eee5bf58ba288af5fa33982a59a63b7b
Oriol Soler <oriol.soler@idiada.com>
2022-05-10 12:26:02 +0200

    #1337 Improvements

--- src/main/java/thekweakestrowsinamatrix/Solution.java
+++ src/main/java/thekweakestrowsinamatrix/Solution.java
@@ -1,12 +1,42 @@
 package thekweakestrowsinamatrix;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
 /**
- * Code problem #1337 https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+ * Code problem #1337
+ * <a href="https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/">https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/</a>
  */
 public class Solution {
 
+    public static class RowStrInd implements Comparable<RowStrInd> {
+
+        public int str;
+        public int ind;
+        public RowStrInd(int str, int ind){
+            this.str = str;
+            this.ind = ind;
+        }
+
+        @Override
+        public int compareTo(RowStrInd o){
+            return o.str > this.str ? -1 : ((o.str == this.str) && o.ind > this.ind) ? -1 : 1;
+        }
+    }
     public int[] kWeakestRows(int[][] mat, int k) {
-        return null;
+        int[] strArray = new int[mat.length];
+        int[] res = new int[k];
+        PriorityQueue<RowStrInd> pq = new PriorityQueue<>();
+        for(int i=0;i<mat.length;i++){
+            pq.add(new RowStrInd((int) Arrays.stream(mat[i]).filter(x -> x!=0).count(), i));
+        }
+
+        for(int i=0;i<k;i++){
+            res[i] = Objects.requireNonNull(pq.poll()).ind;
+        }
+
+        return res;
     }
 
 }
